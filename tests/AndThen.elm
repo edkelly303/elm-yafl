680d85daa50063c32ebc10595333af399dbc2674
Ed Kelly <edwardjkelly@gmail.com>
2025-05-27 13:52:48 +0100

    Add some andThen tests

---
+++ tests/AndThen.elm
@@ -0,0 +1,97 @@
+module AndThen exposing (..)
+
+import Examples
+import Expect
+import Test
+import Yafl
+
+
+tests : Test.Test
+tests =
+    Test.describe
+        "andThen"
+        [ Test.test
+            "Can use andUpdateField with an andThen-ed field that is within an andMap-ed field and has another andMap-ed field after it"
+            (\() ->
+                let
+                    form =
+                        Yafl.succeed (\x y -> ( x, y ))
+                            |> Yafl.andMap field
+                            |> Yafl.andMap (Yafl.succeed ())
+
+                    field =
+                        Examples.fields.string
+                            |> Yafl.id "xxx"
+                            |> Yafl.andThen Yafl.succeed
+                in
+                form
+                    |> Yafl.init
+                    |> Yafl.andUpdateField field "1"
+                    |> Tuple.first
+                    |> Yafl.submit form
+                    |> Expect.equal (Ok ( "1", () ))
+            )
+        , Test.test
+            "Can use andUpdateField with an andThen-ed field that is within an andMap-ed field"
+            (\() ->
+                let
+                    form =
+                        Yafl.succeed (\x -> x)
+                            |> Yafl.andMap field
+
+                    field =
+                        Examples.fields.string
+                            |> Yafl.id "xxx"
+                            |> Yafl.andThen Yafl.succeed
+                in
+                form
+                    |> Yafl.init
+                    |> Yafl.andUpdateField field "1"
+                    |> Tuple.first
+                    |> Yafl.submit form
+                    |> Expect.equal (Ok "1")
+            )
+        , Test.test
+            "Can use andUpdateField with an andThen-ed field that is within an andMap-ed field and has another andMap-ed field before it"
+            (\() ->
+                let
+                    form =
+                        Yafl.succeed (\x y -> ( x, y ))
+                            |> Yafl.andMap (Yafl.succeed ())
+                            |> Yafl.andMap field
+
+                    field =
+                        Examples.fields.string
+                            |> Yafl.id "xxx"
+                            |> Yafl.andThen Yafl.succeed
+                in
+                form
+                    |> Yafl.init
+                    |> Yafl.andUpdateField field "1"
+                    |> Tuple.first
+                    |> Yafl.submit form
+                    |> Expect.equal (Ok ( (), "1" ))
+            )
+        , Test.test
+            "Can use andUpdateField with an andThen-ed field that is within an andMap-ed field and has another andMap-ed field before and after it"
+            (\() ->
+                let
+                    form =
+                        Yafl.succeed (\x y z -> ( x, y, z ))
+                            |> Yafl.andMap (Yafl.succeed ())
+                            |> Yafl.andMap field
+                            |> Yafl.andMap (Yafl.succeed ())
+
+                    field =
+                        Examples.fields.string
+                            |> Yafl.id "xxx"
+                            |> Yafl.andThen Yafl.succeed
+                in
+                form
+                    |> Yafl.init
+                    |> Yafl.andUpdateField field "1"
+                    |> Tuple.first
+                    |> Yafl.submit form
+                    |> Expect.equal (Ok ( (), "1", () ))
+            )
+        ]
